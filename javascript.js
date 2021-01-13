ee7397554371cfcde6ba09db05f6785855287a7a
beni wonka <57801294+beniwonka@users.noreply.github.com>
2021-01-13 20:36:30 +0100

    hide/show divs

--- javascript.js
+++ javascript.js
@@ -102,5 +102,12 @@ function finaleOpen() {
   }
 }
 
-
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else {
+          e.style.display = 'block';
+       }
+    }
 
