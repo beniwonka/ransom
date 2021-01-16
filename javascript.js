e4b1413456f4ec5a3176a24f07062848a0aa5ebd
beni wonka <57801294+beniwonka@users.noreply.github.com>
2021-01-16 11:48:19 +0100

    toggle update + icon update

--- javascript.js
+++ javascript.js
@@ -1,32 +1,3 @@
-let antwort = {  
-  "21" : "Schön dich zu sehen, keine Sorge… ich mach es dir nicht mehr so schwer… Hier findest du die Lösung.<br> Kennst du mich, so freut es dich. <br> Kennst du mich nicht, so suche mich: <br> Du findest mich ganz sicherlich. <br> Ich bin des Rätsels Lösung",
-}
-
-function unknownRedet() {
-  let eingabe = document.getElementById("userBox").value;
-  document.getElementById("userBox").value = "";
-  document.getElementById("chatLog").innerHTML += "Du: " + eingabe + "<br>";
-  if (eingabe in antwort) {
-  document.getElementById("chatLog").innerHTML += "Unknown: " + antwort[eingabe] + "<br>";
-    } else {
-    document.getElementById("chatLog").innerHTML += "Unknown: Ich warte auf die Loesung." + "<br>";
-    }
-}
-
-
-function check_password (input_element) {
-  
-  var password = input_element.value;
-  
-  if (password == '3301')
-      	document.getElementById ('textdatei').style.display = 'block',
-  		  document.getElementById ('passwordzwei').style.display = 'none';
-  else {
-      	document.getElementById ('textdatei').style.display = 'none';
-}
-}
-
-
 function googleOpen() {
   var a = document.getElementById("googleBrowser");
     if (a.style.display === "none") {
@@ -35,19 +6,6 @@ function googleOpen() {
       a.style.display = "block";
       }
     }
-
-function check_google (input_element) {
-  
-  var googleEingabe = input_element.value;
-
-  if (googleEingabe == 'Leetspeek') {
-        document.getElementById ('leet').style.display = 'block';
-        document.getElementById ('falsch').style.display = 'none'; }
-  else {
-        document.getElementById ('falsch').style.display = 'block';
-        document.getElementById ('leet').style.display = 'none';
-}
-}
 
 function check_deactivate (input_element) {
   
@@ -65,6 +23,7 @@ function einsoeffnen() {
   var x = document.getElementById("einsoeffnen");
   if (x.style.display === "none") {
     x.style.display = "block";
+    document.getElementById("ordner").src="icons/ordner_clicked.png";
   } else {
     x.style.display = "none";
   }
@@ -75,6 +34,7 @@ function passwordzwei() {
 	var y = document.getElementById("passwordzwei");
 		if (y.style.display === "none") {
 			y.style.display = "block";
+			document.getElementById("zip").src="icons/zip_clicked.png";
 				} else {
 			y.style.display = "block";
 			}
@@ -99,12 +59,65 @@ function finaleOpen() {
   }
 }
 
-function toggle_visibility(id) {
+function toggle_visibility(id, icon) {
        var e = document.getElementById(id);
-       if(e.style.display == 'block')
+       var f = document.getElementById(icon); 
+       if(e.style.display == 'block') {
           e.style.display = 'none';
+          f.src="icons/" + icon + ".png";
+       }
        else {
           e.style.display = 'block';
        }
     }
+
+/*function changeImageFolder(id) {
+       var image = document.getElementById(id);
+       if(image.style.display == 'block') {
+       
+    } else {
+    	document.getElementById("ordner").src="icons/ordner.png";
+    }
+}*/
+
+
+let antwort = {  
+  "21" : "Schön dich zu sehen, keine Sorge… ich mach es dir nicht mehr so schwer… Hier findest du die Lösung.<br> Kennst du mich, so freut es dich. <br> Kennst du mich nicht, so suche mich: <br> Du findest mich ganz sicherlich. <br> Ich bin des Rätsels Lösung",
+}
+
+function unknownRedet() {
+  let eingabe = document.getElementById("userBox").value;
+  document.getElementById("userBox").value = "";
+  document.getElementById("chatLog").innerHTML += "Du: " + eingabe + "<br>";
+  if (eingabe in antwort) {
+  document.getElementById("chatLog").innerHTML += "Unknown: " + antwort[eingabe] + "<br>";
+    } else {
+    document.getElementById("chatLog").innerHTML += "Unknown: Ich warte auf die Loesung." + "<br>";
+    }
+}
+
+function check_password (input_element) {
+  
+  var password = input_element.value;
+  
+  if (password == '3301')
+      	document.getElementById ('textdatei').style.display = 'block',
+  		  document.getElementById ('passwordzwei').style.display = 'none';
+  else {
+      	document.getElementById ('textdatei').style.display = 'none';
+	}
+}
+
+function check_google (input_element) {
+  
+  var googleEingabe = input_element.value;
+
+  if (googleEingabe == 'Leetspeak') {
+        document.getElementById ('leet').style.display = 'block';
+        document.getElementById ('falsch').style.display = 'none'; }
+  else {
+        document.getElementById ('falsch').style.display = 'block';
+        document.getElementById ('leet').style.display = 'none';
+	}
+}
 
